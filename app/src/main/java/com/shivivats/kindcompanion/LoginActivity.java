f6dde20ce9eb20c44a9a727e96affff807f290eb
SV <shivivats7@gmail.com>
2020-07-23 23:48:17 +0200

    built first release file

--- app/src/main/java/com/shivivats/kindcompanion/LoginActivity.java
+++ app/src/main/java/com/shivivats/kindcompanion/LoginActivity.java
@@ -2,11 +2,16 @@ package com.shivivats.kindcompanion;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 import androidx.preference.PreferenceManager;
@@ -18,65 +23,107 @@ public class LoginActivity extends AppCompatActivity {
     private BiometricPrompt biometricPrompt;
     private BiometricPrompt.PromptInfo promptInfo;
 
+    Button appLoginButton;
+    EditText appLoginPinEntry;
+    TextView appLoginPinMessageText;
+    Button appLoginPinButton;
+
+
+    boolean biometricAvailable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_login);
 
+        appLoginButton = findViewById(R.id.appLoginButton);
+        appLoginPinEntry = findViewById(R.id.appLoginPinEntry);
+        appLoginPinMessageText = findViewById(R.id.appLoginPinMessageText);
+        appLoginPinButton = findViewById(R.id.appLoginPinButton);
+
         //createNotificationChannel();
 
-        Executor executor = ContextCompat.getMainExecutor(this);
-        biometricPrompt = new BiometricPrompt(LoginActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
-            @Override
-            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
-                Toast.makeText(getApplicationContext(),
-                        "Authentication error: " + errString, Toast.LENGTH_SHORT)
-                        .show();
-            }
+        biometricAvailable = false;
 
-            @Override
-            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-                Toast.makeText(getApplicationContext(),
-                        "Authentication succeeded!", Toast.LENGTH_SHORT).show();
-                if (getIntent().getBooleanExtra("FROM_NOTIFICATION", false)) {
-                    // we got here from the notification
-                    LoadReminderNoteList();
-                } else {
-                    LoadFrontPage();
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()) {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                biometricAvailable = true;
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                biometricAvailable = false;
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                biometricAvailable = false;
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                biometricAvailable = false;
+                break;
+        }
+
+        if (biometricAvailable) {
+            Executor executor = ContextCompat.getMainExecutor(this);
+            biometricPrompt = new BiometricPrompt(LoginActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+                @Override
+                public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                    super.onAuthenticationError(errorCode, errString);
+                    Toast.makeText(getApplicationContext(),
+                            "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                            .show();
                 }
-            }
 
-            @Override
-            public void onAuthenticationFailed() {
-                super.onAuthenticationFailed();
-                Toast.makeText(getApplicationContext(), "Authentication failed",
-                        Toast.LENGTH_SHORT)
-                        .show();
-            }
-        });
+                @Override
+                public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                    super.onAuthenticationSucceeded(result);
+                    Toast.makeText(getApplicationContext(),
+                            "Authentication succeeded!", Toast.LENGTH_SHORT).show();
+                    LogIntoApp();
+                }
 
-        promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Biometric login for my app")
-                .setSubtitle("Log in using your biometric credential")
-                //.setNegativeButtonText("Use account password")
-                .setConfirmationRequired(false)
-                .setDeviceCredentialAllowed(true)
-                .build();
+                @Override
+                public void onAuthenticationFailed() {
+                    super.onAuthenticationFailed();
+                    Toast.makeText(getApplicationContext(), "Authentication failed",
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+
+            promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Biometric login for Kind Companion")
+                    .setSubtitle("Log in using your biometric credential")
+                    //.setNegativeButtonText("Use account password")
+                    .setConfirmationRequired(false)
+                    .setDeviceCredentialAllowed(true)
+                    .build();
+        }
 
         // Prompt appears when user clicks "Log in".
         // Consider integrating with the keystore to unlock cryptographic operations,
         // if needed by your app.
-        Button biometricLoginButton = findViewById(R.id.biometricLoginButton);
-        biometricLoginButton.setOnClickListener(view -> {
-            if (PreferenceManager.getDefaultSharedPreferences(this /* Activity context */).getBoolean("biometrics", false) == true) {
+        Button appLoginButton = findViewById(R.id.appLoginButton);
+        appLoginButton.setOnClickListener(view -> {
+            if (PreferenceManager.getDefaultSharedPreferences(this /* Activity context */).getBoolean("biometrics", false) && biometricAvailable) {
                 biometricPrompt.authenticate(promptInfo);
+            } else if (PreferenceManager.getDefaultSharedPreferences(this /* Activity context */).getBoolean("useLoginPin", false)) {
+                appLoginButton.setVisibility(View.INVISIBLE);
+                appLoginPinEntry.setVisibility(View.VISIBLE);
+                appLoginPinMessageText.setVisibility(View.INVISIBLE);
+                appLoginPinButton.setVisibility(View.VISIBLE);
             } else {
-                LoadFrontPage();
+                LogIntoApp();
             }
         });
+    }
+
+    private void LogIntoApp() {
+        if (getIntent().getBooleanExtra("FROM_NOTIFICATION", false)) {
+            // we got here from the notification
+            LoadReminderNoteList();
+        } else {
+            LoadFrontPage();
+        }
     }
 
     private void LoadFrontPage() {
@@ -89,5 +136,15 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+
+    public void checkPinCorrect(View view) {
+        String currentAppPin = PreferenceManager.getDefaultSharedPreferences(this /* Activity context */).getString("appPin", "1234");
+
+        if (appLoginPinEntry != null && TextUtils.equals(appLoginPinEntry.getText(), currentAppPin)) {
+            LogIntoApp();
+        } else {
+            appLoginPinMessageText.setVisibility(View.VISIBLE);
+        }
+    }
 
 }
---
+++ app/src/main/java/com/shivivats/kindcompanion/PinLoginDialogFragment.java
@@ -0,0 +1,60 @@
+package com.shivivats.kindcompanion;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class PinLoginDialogFragment extends DialogFragment {
+
+
+    // Use this instance of the interface to deliver action events
+    PinLoginListener listener;
+
+    // Override the Fragment.onAttach() method to instantiate the listener
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            listener = (PinLoginListener) context;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            e.printStackTrace();
+        }
+
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.dialog_app_pin_login, null))
+                .setTitle("Pin Login")
+                .setPositiveButton("Login", null)
+                .setNegativeButton("Cancel", null);
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+
+    /* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+    public interface PinLoginListener {
+        void onPinLoginDialogPositiveClick(DialogFragment dialog);
+
+        void onPinLoginDialogNegativeClick(DialogFragment dialog);
+    }
+}
